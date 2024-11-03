9123785e275cd34759df1edc67ad72232ef709c4
RooK| <anton.smishko2014@gmail.com>
2024-11-03 14:05:35 +0200

    add-all-components-without-styles

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+const TransactionHistory = ({ data }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.type}</td>
+                        <td>{item.amount}</td>
+                        <td>{item.currency}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default TransactionHistory;
